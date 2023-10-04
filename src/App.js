70a0b6ae102b501ccbd47b4626ccd5a081891335
Ali Enes Yaman <92275400+alienesyaman@users.noreply.github.com>
2023-10-04 15:28:56 +0300

    card-2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Card2 from './components/Card2';
+
+function App() {
+  return (
+    <div>
+     
+<Card2></Card2>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card2.js
@@ -0,0 +1,130 @@
+import React from 'react'
+import "../components/Card2.css"
+import img1 from "../image/unnamed.png"
+
+
+function Card2() {
+  return (
+    <div>
+      
+      <div class="grid-container">
+
+<div className='grid-1'>
+  <div className='grid-1-img'>
+  <img src={img1}></img>
+</div>
+  <div className='grid-1-text'>
+  <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p className='p-botom'><div className='box'></div> R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+</div>
+</div>
+
+
+<div className='grid-1'>
+  <div className='grid-1-img'>
+  <img src={img1}></img>
+</div>
+  <div className='grid-1-text'>
+  <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p className='p-botom'><div className='box'></div> R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+</div>
+</div>
+
+
+<div className='grid-1'>
+  <div className='grid-1-img'>
+  <img src={img1}></img>
+</div>
+  <div className='grid-1-text'>
+  <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p className='p-botom'><div className='box'></div> R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+</div>
+</div>
+
+
+<div className='grid-1'>
+  <div className='grid-1-img'>
+  <img src={img1}></img>
+</div>
+  <div className='grid-1-text'>
+  <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p className='p-botom'><div className='box'></div> R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+</div>
+</div>
+
+
+<div className='grid-1'>
+  <div className='grid-1-img'>
+  <img src={img1}></img>
+</div>
+  <div className='grid-1-text'>
+  <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p className='p-botom'><div className='box'></div> R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+</div>
+</div>
+
+
+<div className='grid-1'>
+  <div className='grid-1-img'>
+  <img src={img1}></img>
+</div>
+  <div className='grid-1-text'>
+  <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p className='p-botom'><div className='box'></div> R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+</div>
+</div>
+
+
+<div className='grid-1'>
+  <div className='grid-1-img'>
+  <img src={img1}></img>
+</div>
+  <div className='grid-1-text'>
+  <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p className='p-botom'><div className='box'></div> R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+</div>
+</div>
+
+
+<div className='grid-1'>
+  <div className='grid-1-img'>
+  <img src={img1}></img>
+</div>
+  <div className='grid-1-text'>
+  <p>Stefan Kuntz'a ödenecek
+tazminat belli oldu</p>
+<div className='line'></div>
+<div className='bottom-top'><p>Haber Kaynağı</p><p>Paylaşım T.</p> </div>
+<div className='bottom-bottom'><p className='p-botom'><div className='box'></div> R.Pehlivanlar</p><p>29 Aralık 2023</p> </div>
+</div>
+</div>
+
+
+</div>  
+
+    </div>
+  )
+}
+
+export default Card2
